e825d4ea6fcf29aa50be18e0df89298121b0a9fd
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-05 12:43:37 +0100

    Added product structure

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,11 +1,49 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet, Image } from "react-native"
 
-const Product=(productData)=> {
+const Product=({productData})=> {
     return (
-       <View>
+       <View style={style.container}>
+        {/* <Image source={{uri:`https://api.timbu.cloud/images/${productData.photos[0].url}`}}/>
+        <Image 
+                    source={{ uri: `https://api.timbu.cloud/images/shoptackle/product_hdmi_adapter_15a595_1.jpg` }} 
+                /> */}
+                {productData.photos && productData.photos[0]?(
+                    <Image style={style.image} source={{uri:`https://api.timbu.cloud/images/${productData.photos[0].url}`}}/>
+                ):
+                (
+                    <View style={style.imagePlaceHolder}></View>
+                )
+
+                }
         <Text>{productData.name}</Text>
+        <Text>₦{productData.current_price[0].NGN}</Text>
        </View>
     );
 }
 
+const style = StyleSheet.create({
+    container: {
+        position: "relative",
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: 2,
+        borderBottomRightRadius: 2,
+        borderWidth: 1,
+        borderColor: "#000",
+        width: "48%",
+        padding: 1,
+        alignItems: 'center',
+    },
+    image:{
+        width:"100%",
+        height: 200,
+    },
+    imagePlaceHolder:{
+        width: '100%',
+        height: 200,
+        backgroundColor: '#ccc',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+})
 export default Product;
