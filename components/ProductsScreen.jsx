d0f4c9e769a425b47f56da1515121a56c6d3c688
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-05 12:44:16 +0100

    Added products

--- components/ProductsScreen.jsx
+++ components/ProductsScreen.jsx
@@ -1,30 +1,46 @@
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native"
+import { Text, View, ScrollView, StyleSheet } from "react-native"
 import Product from "./Product";
 
 
-const  ProductsScreen=()=> {
-    const  [storeData, setStoreData] = useState({items:[]})
-    const fetchStoreData=async()=>{
-        let storeDataJson=await fetch("https://api.timbu.cloud/products?organization_id=4da78e0dd7ab42988da4e8014490cff7&Appid=67HGWYE946GG1CI&Apikey=5994a513c2084c5e9fc5294a8ee5908620240705095509566488&reverse_sort=false&page=1&size=10").then((response)=>response.json());
+const ProductsScreen = () => {
+    const [storeData, setStoreData] = useState({ items: [] })
+    const fetchStoreData = async () => {
+        let storeDataJson = await fetch("https://api.timbu.cloud/products?organization_id=4da78e0dd7ab42988da4e8014490cff7&Appid=67HGWYE946GG1CI&Apikey=5994a513c2084c5e9fc5294a8ee5908620240705095509566488&reverse_sort=false&page=1&size=19").then((response) => response.json());
         return storeDataJson;
     }
     const getStoreData = async () => {
         const data = await fetchStoreData();
         setStoreData(data);
     }
-    useEffect(()=>{ 
-
+    useEffect(() => {
         getStoreData();
 
-    },[])
+    }, [])
     return (
-       <View>
-        {storeData.items.map((item)=>(
-            <Product key={item.id} ProductData={item}/>
-        ))}
-        </View>
+        <ScrollView contentContainerStyle={styles.productsContainer}>
+            <View style={styles.contentContainer}>
+                <Text style={{ textAlign: "left", width: "95%" }}>Products on shelf</Text>
+                {storeData.items.map((item) => (
+                    <Product productData={item} key={item.id} />
+                ))}
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    productsContainer: {
+        alignItems: "center",
+        marginHorizontal:"auto",
+        width: "95%",
+    },
+    contentContainer: {
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        gap: 10,
+        flexDirection: "row",
+    }
+})
 
 export default ProductsScreen;
