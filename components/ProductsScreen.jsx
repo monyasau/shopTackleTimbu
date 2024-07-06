f8016e273c0dae6363d20cae89632c2e5f5fc9ca
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-06 11:30:40 +0100

    Added ground work to Increase skeleton count

--- components/ProductsScreen.jsx
+++ components/ProductsScreen.jsx
@@ -31,6 +31,7 @@ const ProductsScreen = () => {
         loading ?
         <ScrollView contentContainerStyle={styles.productsContainer}>
                 <View style={styles.contentContainer}>
+                    [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
                     <SkeletonProduct/>
                     <SkeletonProduct/>
                     <SkeletonProduct/>
