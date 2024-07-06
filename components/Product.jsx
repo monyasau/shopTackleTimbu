586d62f11c53018972806fb6f3248484aeeb20fe
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-06 13:55:59 +0100

    Fixed modal design

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,44 +1,124 @@
-import { View, Text, StyleSheet, Image } from "react-native"
+import { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from "react-native"
+import { AntDesign } from '@expo/vector-icons';
 
-const Product=({productData})=> {
+const ProductModal = ({ active, setActive, productData }) => {
+    if (active) {
+        return null;
+    }
+    console.log(active);
     return (
-       <View style={style.container}>
-        {/* <Image source={{uri:`https://api.timbu.cloud/images/${productData.photos[0].url}`}}/>
-        <Image 
-                    source={{ uri: `https://api.timbu.cloud/images/shoptackle/product_hdmi_adapter_15a595_1.jpg` }} 
-                /> */}
-                {productData.photos && productData.photos[0]?(
-                    <Image style={style.image} source={{uri:`https://api.timbu.cloud/images/${productData.photos[0].url}`}}/>
-                ):
-                (
-                    <View style={style.imagePlaceHolder}></View>
-                )
+        <Modal
+            transparent onRequestClose={() => setActive(true)}
+            animationType="slide">
+            <View style={style.modalSurrounding}>
+                <View style={style.modalContainer}>
+                    <TouchableOpacity style={style.close} onPress={()=>setActive(!active)}>
+                    <AntDesign name="close"size={24} color="black" />
+                        <Text>Close</Text>
+                    </TouchableOpacity>
+                    {productData.photos && productData.photos[0] ? (
+                        <Image style={style.modalImage} source={{ uri: `https://api.timbu.cloud/images/${productData.photos[0].url}` }} />
+                    ) :
+                        (
+                            <View style={style.imagePlaceHolder}></View>
+                        )
 
-                }
-        <Text>{productData.name}</Text>
-        <Text>₦{productData.current_price[0].NGN}</Text>
-       </View>
+                    }
+                    <View style={style.modalInfoContainer}>
+
+                        <Text >{productData.name}</Text>
+                        <Text style={{ fontWeight: 700 }}>₦ {productData.current_price[0].NGN}</Text>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+}
+
+
+const Product = ({ productData }) => {
+    const [modalActive, setModalActive] = useState(true)
+    return (
+        <>
+            <TouchableOpacity style={style.container} onPress={() => setModalActive(false)}>
+                <View>
+                    {productData.photos && productData.photos[0] ? (
+                        <Image style={style.image} source={{ uri: `https://api.timbu.cloud/images/${productData.photos[0].url}` }} />
+                    ) :
+                        (
+                            <View style={style.imagePlaceHolder}></View>
+                        )
+
+                    }
+                    <View style={style.infoContainer}>
+
+                        <Text >{productData.name}</Text>
+                        <Text style={{ fontWeight: 700 }}>₦ {productData.current_price[0].NGN}</Text>
+                    </View>
+                </View>
+            </TouchableOpacity>
+            <ProductModal productData={productData} setActive={setModalActive} active={modalActive} />
+        </>
     );
 }
 
 const style = StyleSheet.create({
+
     container: {
         position: "relative",
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
         borderBottomLeftRadius: 2,
         borderBottomRightRadius: 2,
         borderWidth: 1,
         borderColor: "#000",
         width: "48%",
         padding: 1,
-        alignItems: 'center',
     },
-    image:{
-        width:"100%",
+    infoContainer: {
+        padding: 2,
+        justifyContent: "space-between",
+        flex: 1
+    },
+    image: {
+        width: "100%",
         height: 200,
     },
-    imagePlaceHolder:{
+    modalSurrounding: {
+        flex: 1,
+        backgroundColor: "#00000070",
+        paddingHorizontal: "5%",
+        justifyContent: "center",
+    },
+    modalContainer: {
+    },
+    modalInfoContainer: {
+        marginVertical:10,
+        borderRadius:8,
+        padding: 4,
+        backgroundColor:"#cccccc"
+    },
+    modalImage: {
+        borderWidth: 1,
+        borderRadius: 6,
+        width: "100%",
+        marginHorizontal: "auto",
+        height: "75%",
+    },
+    close:{
+        marginVertical:10,
+        marginLeft:"auto",
+        flexDirection:"row",
+         borderWidth:1,
+         borderColor:"red",
+         backgroundColor:"white",
+         borderRadius:100,
+         padding:10,
+         alignItems:"center"
+
+    },
+    imagePlaceHolder: {
         width: '100%',
         height: 200,
         backgroundColor: '#ccc',
