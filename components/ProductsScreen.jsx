27b2389929214a034347dcac9555698225f223c7
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-06 11:28:19 +0100

    Added skeleton for loading state

--- components/ProductsScreen.jsx
+++ components/ProductsScreen.jsx
@@ -1,46 +1,76 @@
 import { useEffect, useState } from "react";
 import { Text, View, ScrollView, StyleSheet } from "react-native"
 import Product from "./Product";
+import SkeletonProduct from './SkeletonProduct';
 
 
 const ProductsScreen = () => {
     const [storeData, setStoreData] = useState({ items: [] })
-    const fetchStoreData = async () => {
-        let storeDataJson = await fetch("https://api.timbu.cloud/products?organization_id=4da78e0dd7ab42988da4e8014490cff7&Appid=67HGWYE946GG1CI&Apikey=5994a513c2084c5e9fc5294a8ee5908620240705095509566488&reverse_sort=false&page=1&size=19").then((response) => response.json());
-        return storeDataJson;
-    }
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
     const getStoreData = async () => {
-        const data = await fetchStoreData();
-        setStoreData(data);
+        try {
+            let storeDataJson = await fetch("https://api.timbu.cloud/products?organization_id=4da78e0dd7ab42988da4e8014490cff7&Appid=67HGWYE946GG1CI&Apikey=5994a513c2084c5e9fc5294a8ee5908620240705095509566488&reverse_sort=false&page=1&size=19").then((response) => response.json());
+            const data = await storeDataJson;
+            setStoreData(data);
+            setError(false)
+            setLoading(true)
+        }
+        catch (error) {
+            console.log(error);
+            setError(true)
+            setLoading(false)
+        }
     }
     useEffect(() => {
         getStoreData();
+    }, []);
 
-    }, [])
     return (
+
+        loading ?
         <ScrollView contentContainerStyle={styles.productsContainer}>
-            <View style={styles.contentContainer}>
-                <Text style={{ textAlign: "left", width: "95%" }}>Products on shelf</Text>
-                {storeData.items.map((item) => (
-                    <Product productData={item} key={item.id} />
-                ))}
-            </View>
+                <View style={styles.contentContainer}>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                    <SkeletonProduct/>
+                </View>
         </ScrollView>
-    );
+                :
+                <ScrollView contentContainerStyle={styles.productsContainer}>
+                    <View style={styles.contentContainer}>
+                        <Text style={{ textAlign: "left", width: "95%" }}>Products on shelf</Text>
+                        {storeData.items.map((item) => (
+                            <Product productData={item} key={item.id} />
+                        ))}
+                    </View>
+                </ScrollView>
+
+                );
 }
 
-const styles = StyleSheet.create({
-    productsContainer: {
-        alignItems: "center",
-        marginHorizontal:"auto",
-        width: "95%",
+                const styles = StyleSheet.create({
+                    productsContainer: {
+                    alignItems: "center",
+                marginHorizontal: "auto",
+                width: "95%",
+                paddingBottom: 10,
     },
-    contentContainer: {
-        justifyContent: "space-between",
-        flexWrap: "wrap",
-        gap: 10,
-        flexDirection: "row",
+                contentContainer: {
+                    justifyContent: "space-between",
+                flexWrap: "wrap",
+                gap: 10,
+                flexDirection: "row",
     }
 })
 
-export default ProductsScreen;
+                export default ProductsScreen;
