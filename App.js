41564239a3d9e400bd6f7eb95c8a65db270fcf81
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-07 07:05:24 +0100

    Added topbar with reload functionality

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import ProductsScreen from "./components/ProductsScreen";
+import BottomNavigation from "./components/TopBar";
+import TopBar from "./components/TopBar";
+import { useState } from "react";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [reloadStore, setReloadStore] = useState(false);
+  const reloadStoreFunction = () => {
+    setReloadStore(!reloadStore);
+  };
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="ProductsScreen">
-          <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <TopBar reloadStore={reloadStoreFunction} />
+      <ProductsScreen reloadStore={reloadStore} />
       <StatusBar style="auto" />
     </>
   );
@@ -26,9 +30,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
-// 5994a513c2084c5e9fc5294a8ee5908620240705095509566488
-// 5994a513c2084c5e9fc5294a8ee5908620240705095509566488
-// 67HGWYE946GG1CI
-// org id: 4da78e0dd7ab42988da4e8014490cff7
-// https://api.timbu.cloud/products?organization_id=4da78e0dd7ab42988da4e8014490cff7&Appid=67HGWYE946GG1CI&Apikey=5994a513c2084c5e9fc5294a8ee5908620240705095509566488&reverse_sort=false&page=1&size=10
